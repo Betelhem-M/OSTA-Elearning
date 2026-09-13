a121d28f0f68ea1dc5015f3a798ea8c9847777a0
Betty <betelmolaw@gmail.com>
2026-09-13 12:34:54 +0300

    feat: show course content editor in workspace

--- frontend/src/layouts/InstructorLayout.jsx
+++ frontend/src/layouts/InstructorLayout.jsx
@@ -1,56 +1,32 @@
-import {
-  Outlet,
-  Link,
-} from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Sidebar from "@components/layout/Sidebar";
 import Navbar from "@components/layout/Navbar";
+import CourseContentEditor from "@components/course/CourseContentEditor";
 
-import {
-  INSTRUCTOR_SIDEBAR_NAV,
-} from "@constants/navigation";
-
-import {
-  useSidebarDrawer,
-} from "@hooks/useSidebarDrawer";
-
-import {
-  useAuth,
-} from "@context/AuthContext";
+import { INSTRUCTOR_SIDEBAR_NAV } from "@constants/navigation";
+import { useSidebarDrawer } from "@hooks/useSidebarDrawer";
 
 export default function InstructorLayout() {
-  const drawer =
-    useSidebarDrawer();
+  const drawer = useSidebarDrawer();
+  const location = useLocation();
 
-  const {
-    user,
-  } = useAuth();
+  const isCourseWorkspace = /^\/instructor\/courses\/\d+$/.test(location.pathname);
 
   return (
     <div className="min-h-screen bg-surface font-sans text-ink dark:bg-structure dark:text-white">
-      {/* SIDEBAR */}
-
       <Sidebar
-        navItems={
-          INSTRUCTOR_SIDEBAR_NAV
-        }
-        isOpen={
-          drawer.isOpen
-        }
-        onClose={
-          drawer.close
-        }
+        navItems={INSTRUCTOR_SIDEBAR_NAV}
+        isOpen={drawer.isOpen}
+        onClose={drawer.close}
         subtitle="Instructor workspace"
       />
 
-      {/* HEADER */}
-
       <Navbar onMenuOpen={drawer.open} />
-
-      {/* CONTENT */}
 
       <main className="pb-8 lg:pl-64">
         <div className="mx-auto max-w-6xl px-4 py-6">
+          {isCourseWorkspace && <CourseContentEditor />}
           <Outlet />
         </div>
       </main>
